994412c62830c77e23c4e75f45f8e501bb715abf
Axel Sirota <asirota@asapp.com>
2019-01-06 16:58:09 -0300

    Fixing test

--- tests/units/test_joke.py
+++ tests/units/test_joke.py
@@ -1,9 +1,9 @@
 import unittest
-import app
+from app.app import get_joke
 
 
 class TestJoke(unittest.TestCase):
 
     def test_joke(self):
-        joke = app.get_joke()
+        joke = get_joke()
         self.assertEqual(isinstance(joke, str), True)
